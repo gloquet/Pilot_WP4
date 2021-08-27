8bf15fb44571473cd1130ffff48c358897f16772
Gérard <44569291+gloquet@users.noreply.github.com>
2021-08-28 01:03:14 +1000

    Add files via upload

--- FactorAnalysis.R
+++ FactorAnalysis.R
@@ -8,7 +8,7 @@ library(RColorBrewer)
 
 
 # Data path
-data.path <- "C:/Users/loquetg/Documents/Data/"
+data.path <- "C:/Users/loquetg/Documents/DataSet/"
 
 # Load data
 data.right <- read.xlsx(paste0(data.path,"DATA_All_Norm_R_V6.xlsx"),1)
@@ -17,8 +17,7 @@ data.right <- read.xlsx(paste0(data.path,"DATA_All_Norm_R_V6.xlsx"),1)
 # Factor analysis
 # Headers: PTA, DS, SRT, Region, Sex, Age, HINT_V2, HINT_bin, ACALOS_HTL, ACALOS_MCL, ACALOS_UCL, ACALOS_slope, STM,
 #          IPD, BINP_diop, BINP_dichop, BIN_totp, BIN_falsep, HA, HINT_V3, IDENTIF_V3, NOISE_ANN_V3, HEAD_DIR_V3,
-#          HEAD_W_V3, JFC_V3, IG55_V3, IG65_V3, IG80_V3, HINT_V4, IDENTIF_V4, NOISE_ANN_V4, DIRECTION_V4, HEAD_DIR_V4,
-#          HEAD_W_V4, JFC_V4
+#          HEAD_W_V3, JFC_V3, IG55_V3, IG65_V3, IG80_V3, HINT_V4, IDENTIF_V4, NOISE_ANN_V4, HEAD_DIR_V4, HEAD_W_V4, JFC_V4
 data.right.source <- data.right %>% select(c(3,4,5,6,7,8,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37))
 data.right.cor <- cor(data.right.source, use = 'complete.obs')
 
@@ -63,7 +62,7 @@ colnames(Right_plot) <- x
 x_axis_lab <- c('PTA','DS','SRT','Region','Sex','Age','HINT_V2','HINT_bin','ACALOS_HTL','ACALOS_MCL','ACALOS_UCL',
                 'ACALOS_slope','STM','IPD','BINP_diop','BINP_dichop','BIN_totp','BIN_falsep','HA','HINT_V3',
                 'IDENTIF_V3','NOISE_ANN_V3','HEAD_DIR_V3','HEAD_W_V3','JFC_V3','IG55_V3','IG65_V3','IG80_V3',
-                'HINT_V4','IDENTIF_V4','NOISE_ANN_V4','DIRECTION_V4','HEAD_DIR_V4','HEAD_W_V4','JFC_V4')
+                'HINT_V4','IDENTIF_V4','NOISE_ANN_V4','HEAD_DIR_V4','HEAD_W_V4','JFC_V4')
 
 colnames(data.right.source) <- x_axis_lab 
 
@@ -71,14 +70,14 @@ colnames(data.right.source) <- x_axis_lab
 Right_plot$Item <- rep(colnames(data.right.source),9)
 Right_plot$Loading <- as.vector(fa.right$Structure)
 Right_plot$Factors <- c(rep('Factor 1',34),
-                      rep('Factor 2',35),
-                      rep('Factor 3',35),
-                      rep('Factor 4',35),
-                      rep('Factor 5',35),
-                      rep('Factor 6',35),
-                      rep('Factor 7',35),
-                      rep('Factor 8',35),
-                      rep('Factor 9',35))
+                      rep('Factor 2',34),
+                      rep('Factor 3',34),
+                      rep('Factor 4',34),
+                      rep('Factor 5',34),
+                      rep('Factor 6',34),
+                      rep('Factor 7',34),
+                      rep('Factor 8',34),
+                      rep('Factor 9',34))
 
 
 #tiff(filename = 'PCA_SSQ.tif',width = 1600, height = 700)
